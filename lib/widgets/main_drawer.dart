eb564d271fb597d38519551c4c0de6fcf1eaa9c3
Emre Atilla <emreanlatilla@gmail.com>
2025-10-07 15:02:23 +0300

    Drawer and ListTile added ... impementing drawer functions

---
+++ lib/widgets/main_drawer.dart
@@ -0,0 +1,81 @@
+import "package:flutter/material.dart";
+
+class MainDrawer extends StatelessWidget {
+  const MainDrawer({super.key, required this.onSelectScreen});
+  final void Function(String identifier) onSelectScreen;
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          DrawerHeader(
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Theme.of(context).colorScheme.primaryContainer,
+                  Theme.of(
+                    context,
+                  ).colorScheme.primaryContainer.withValues(alpha: 0.8),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.fastfood,
+                  size: 62,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                const SizedBox(width: 18),
+                Text(
+                  "Cooking Up !",
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    fontSize: 28,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.restaurant_menu,
+              size: 36,
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+            title: Text(
+              "Meals",
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+                fontSize: 30,
+              ),
+            ),
+            onTap: () {
+              onSelectScreen('meals');
+            },
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.settings_applications,
+              size: 36,
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+            title: Text(
+              "Filters",
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+                fontSize: 30,
+              ),
+            ),
+            onTap: () {
+              onSelectScreen('filters');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
